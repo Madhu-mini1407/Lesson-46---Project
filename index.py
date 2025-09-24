2c3e5cf39d2cf8864ff250d9d84f71d79c151f4c
madhu-mini1407 <krishformadhu@gmail.com>
2025-09-24 14:18:54 +0530

    initialcommit

--- index.py
+++ index.py
@@ -1,25 +1,26 @@
 class Expression:
-    def __init__(self, a, b):
+    def __init__(self, a, b, c):
         self.a = a
         self.b = b
+        self.c = c
 
     def add(self):
-        return self.a + self.b
+        return self.a + self.b + self.c
 
     def subtract(self):
-        return self.a - self.b
+        return self.a - self.b - self.c
 
     def multiply(self):
-        return self.a * self.b
+        return self.a * self.b * self.c
 
     def divide(self):
         if self.b != 0:
-            return self.a / self.b
+            return self.a / self.b/ self.c
         else:
             return "Division by zero is not allowed"
 
     def display(self):
-        print(f"Numbers: {self.a}, {self.b}")
+        print(f"Numbers: {self.a}, {self.b},{self.c}")
         print(f"Addition: {self.add()}")
         print(f"Subtraction: {self.subtract()}")
         print(f"Multiplication: {self.multiply()}")
@@ -27,5 +28,5 @@ class Expression:
 
 
 if __name__ == "__main__":
-    expr = Expression(20, 5)
+    expr = Expression(20, 5,2)
     expr.display()
